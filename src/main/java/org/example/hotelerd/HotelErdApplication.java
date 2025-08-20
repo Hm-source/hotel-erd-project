214eeb34c455d74e083bd422801d76c2b7c52268
Hm-source <swo8852@gmail.com>
2025-08-20 15:23:48 +0900

    first commit

---
+++ src/main/java/org/example/hotelerd/HotelErdApplication.java
@@ -0,0 +1,13 @@
+package org.example.hotelerd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelErdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HotelErdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/hotelerd/HotelErdApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hotelerd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelErdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
