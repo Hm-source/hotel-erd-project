ff344b524390acecaed85ef1659bb1be7107ce9a
Hm-source <swo8852@gmail.com>
2025-08-22 02:17:54 +0900

    feat: 예약 취소 컨트롤러 메서드 구현

--- src/main/java/org/example/hotelerd/controller/reservation/ReservationController.java
+++ src/main/java/org/example/hotelerd/controller/reservation/ReservationController.java
@@ -3,14 +3,18 @@ package org.example.hotelerd.controller.reservation;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import org.example.hotelerd.controller.reservation.dto.ReservationCancelResponseDto;
 import org.example.hotelerd.controller.reservation.dto.ReservationRequestDto;
 import org.example.hotelerd.controller.reservation.dto.ReservationResponseDto;
 import org.example.hotelerd.service.ReservationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -26,5 +30,14 @@ public class ReservationController {
         @RequestBody ReservationRequestDto requestDto) {
         ReservationResponseDto responseDto = reservationService.createReservation(requestDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @DeleteMapping("/{reservationId}")
+    public ResponseEntity<ReservationCancelResponseDto> cancelReservation(
+        @PathVariable Integer reservationId,
+        @RequestParam Integer userId) {
+        ReservationCancelResponseDto responseDto = reservationService.cancelReservation(
+            reservationId, userId);
+        return ResponseEntity.ok(responseDto);
     }
 }
