d22e00186f99576e119d440299c1d2adf211c6a5
Hm-source <swo8852@gmail.com>
2025-08-21 11:56:55 +0900

    feat: Hotel-RoomType 양방향 설정

--- src/main/java/org/example/hotelerd/repository/hotel/entity/Hotel.java
+++ src/main/java/org/example/hotelerd/repository/hotel/entity/Hotel.java
@@ -1,11 +1,15 @@
 package org.example.hotelerd.repository.hotel.entity;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import java.time.LocalTime;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -18,6 +22,7 @@ import lombok.ToString;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Hotel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -25,5 +30,7 @@ public class Hotel {
     private String address;
     private LocalTime checkInTime;
     private LocalTime checkOutTime;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "hotel", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<RoomType> roomTypes;
 
 }
