c3e3c4a06d2faac61258d2c3953d0829e95777c4
Hm-source <swo8852@gmail.com>
2025-08-21 14:07:22 +0900

    feat: 사용하지 않는 메서드 삭제

--- src/main/java/org/example/hotelerd/repository/hotel/RoomTypeRepository.java
+++ src/main/java/org/example/hotelerd/repository/hotel/RoomTypeRepository.java
@@ -1,15 +1,10 @@
 package org.example.hotelerd.repository.hotel;
 
-import java.util.List;
 import org.example.hotelerd.repository.hotel.entity.RoomType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RoomTypeRepository extends JpaRepository<RoomType, Integer> {
 
-    @Query("SELECT rt FROM RoomType rt WHERE rt.hotel.id = :hotelId ORDER BY rt.id")
-    List<RoomType> findByHotelId(@Param("hotelId") Integer hotelId);
 }
