b78b2d115476e25bf932e74de3d684d05f01257b
Hm-source <swo8852@gmail.com>
2025-08-21 13:56:19 +0900

    feat: RoomAvailabilityInfoDto isAvailable 값 추가

--- src/main/java/org/example/hotelerd/controller/hotel/dto/RoomAvailabilityInfoDto.java
+++ src/main/java/org/example/hotelerd/controller/hotel/dto/RoomAvailabilityInfoDto.java
@@ -17,6 +17,7 @@ public class RoomAvailabilityInfoDto {
     private String roomType;
     private String description;
     private Integer price;
+    private Boolean isAvailable;
     private Integer availableRoomCount;
 
     public static RoomAvailabilityInfoDto of(RoomType roomType, RoomDatePrice roomDatePrice) {
@@ -24,6 +25,30 @@ public class RoomAvailabilityInfoDto {
             .roomTypeId(roomType.getId())
             .roomType(roomType.getType())
             .description(roomType.getDescription())
+            .price(roomDatePrice.getPrice())
+            .isAvailable(roomDatePrice.getQuantity() > 0)
+            .availableRoomCount(roomDatePrice.getQuantity())
+            .build();
+    }
+
+    public static RoomAvailabilityInfoDto unavailable(RoomType roomType) {
+        return RoomAvailabilityInfoDto.builder()
+            .roomTypeId(roomType.getId())
+            .roomType(roomType.getType())
+            .description(roomType.getDescription())
+            .isAvailable(false)
+            .price(null)
+            .availableRoomCount(0)
+            .build();
+    }
+
+    public static RoomAvailabilityInfoDto available(RoomType roomType,
+        RoomDatePrice roomDatePrice) {
+        return RoomAvailabilityInfoDto.builder()
+            .roomTypeId(roomType.getId())
+            .roomType(roomType.getType())
+            .description(roomType.getDescription())
+            .isAvailable(true)
             .price(roomDatePrice.getPrice())
             .availableRoomCount(roomDatePrice.getQuantity())
             .build();
