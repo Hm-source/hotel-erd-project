33bce350ad2e9958e282814a29780b40fdf347a7
Hm-source <swo8852@gmail.com>
2025-08-22 14:42:39 +0900

    feat: repository 불필요 코드 제거

--- src/main/java/org/example/hotelerd/repository/reservation/ReservationRepository.java
+++ src/main/java/org/example/hotelerd/repository/reservation/ReservationRepository.java
@@ -1,13 +1,9 @@
 package org.example.hotelerd.repository.reservation;
 
-import java.util.Optional;
 import org.example.hotelerd.repository.reservation.entity.Reservations;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReservationRepository extends JpaRepository<Reservations, Integer> {
 
-    Optional<Reservations> findById(Integer id);
-
-    Reservations save(Reservations entity);
 
 }
--- src/main/java/org/example/hotelerd/repository/user/UserRepository.java
+++ src/main/java/org/example/hotelerd/repository/user/UserRepository.java
@@ -1,6 +1,5 @@
 package org.example.hotelerd.repository.user;
 
-import java.util.Optional;
 import org.example.hotelerd.repository.user.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +7,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<Users, Integer> {
 
-    Optional<Users> findById(Integer id);
 }
